5faa6fd85ec24d7e6fd5c33453668cf6936a67c7
Your Name <you@example.com>
2022-12-13 11:46:21 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/JohnMMac/network/azurerm"
   version = "3.5.0"
-  resource_group_name = azurerm_resource_group.name
+  resource_group_name = myresourcegroup.name
 }
